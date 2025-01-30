64deb5063ce35b78ba4ba34640ad627e0bec774c
abdusharip <abdusharipa9@gmail.com>
2025-01-30 10:16:04 +0500

    Routing #29 - dars

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+// port
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => console.log(`server is running port = ${PORT}`))
