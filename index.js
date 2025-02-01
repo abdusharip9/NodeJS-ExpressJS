86862607d6bcc42752c7be24d9fc3e47de4c167e
abdusharip <abdusharipa9@gmail.com>
2025-02-01 14:19:34 +0500

    #37. MongoDB olash & .env

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import * as dotenv from 'dotenv'
 import express from 'express'
 import { create } from 'express-handlebars'
+
+import mongoose from 'mongoose'
 import AuthRoutes from './routes/auth.js'
 import ProductsRoutes from './routes/products.js'
+dotenv.config()
 
 const app = express()
 
@@ -10,11 +14,29 @@ const hbs = create({ defaultLayout: 'main', extname: 'hbs' })
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
+
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(express.json())
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-// port
-const PORT = process.env.PORT || 4100
-app.listen(PORT, () => console.log(`server is running port = ${PORT}`))
+const startApp = () => {
+	try {
+		mongoose.set('strictQuery', false)
+
+		mongoose
+			.connect(process.env.MONGO_URI)
+			.then(() => console.log('MongoDB connected.'))
+			.catch(err => console.error('MongoDB connection error:', err))
+
+		// port
+		const PORT = process.env.PORT || 4100
+		app.listen(PORT, () => console.log(`server is running port = ${PORT}`))
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+startApp()
