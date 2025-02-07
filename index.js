6f230d8fd7c1a89ae87ea2f2314afa1fd67f2c98
abdusharip <abdusharipa9@gmail.com>
2025-02-07 06:10:57 +0500

    #44. Dynamic navbar, Cookies (save cookies, global token, dynamic logut and navbar)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import flash from 'connect-flash'
+import cookieParser from 'cookie-parser'
 import * as dotenv from 'dotenv'
 import express from 'express'
 import { create } from 'express-handlebars'
 import session from 'express-session'
+import varMiddleware from './middleware/var.js'
 
 import mongoose from 'mongoose'
 import AuthRoutes from './routes/auth.js'
@@ -20,8 +22,10 @@ app.set('views', './views')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 app.use(session({ secret: 'Sammi', resave: false, saveUninitialized: false }))
 app.use(flash())
+app.use(varMiddleware)
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,7 @@ const router = Router()
 router.get('/', (req, res) => {
 	res.render('index', {
 		title: 'Home | Sammi',
+		token: true,
 	})
 })
 router.get('/products', (req, res) => {
